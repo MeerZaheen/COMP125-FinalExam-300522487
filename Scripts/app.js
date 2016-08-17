1923093b3661b7c7de9a8f00875080f481cd725e
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2016-08-17 14:01:10 -0400

    added die and positioned them

--- Scripts/app.js
+++ Scripts/app.js
@@ -20,10 +20,13 @@ var core;
     var stage;
     var rollButton;
     var blank;
+    var blank2;
     var die1;
     var die2;
-    var die1result;
-    var die2result;
+    var die3;
+    var die4;
+    var die5;
+    var die6;
     // app entry function
     function init() {
         canvas = document.getElementById("canvas");
@@ -45,13 +48,41 @@ var core;
     function main() {
 
         rollButton = new createjs.Bitmap("Assets/images/rollButton.png");
-        rollButton.x = 330;
-        rollButton.y = 240;
-        blank = new createjs.Bitmap("Assets/images/blank.png");
-        stage.addChild(blank);
+        rollButton.x = 250;
+        rollButton.y = 350;
         stage.addChild(rollButton);
-        die1 = new createjs.Text(die1);
+        blank2 = new createjs.Bitmap("Assets/images/blank.png");
+                blank2.x = 100;
+        blank2.y = 100;
+                
+        stage.addChild(blank2);
+        blank = new createjs.Bitmap("Assets/images/blank.png");
+        blank.x = 400;
+        blank.y = 100;
+        stage.addChild(blank);
+        die1 = new createjs.Bitmap("Assets/images/1.png");
+        die1.x = 400;
+        die1.y = 100;
         stage.addChild(die1);
+        die2 = new createjs.Bitmap("Assets/images/2.png");
+        die2.x = 400;
+        die2.y = 100;
+        stage.addChild(die2);
+        die3 = new createjs.Bitmap("Assets/images/3.png");
+        die3.x = 400;
+        die3.y = 100;
+        stage.addChild(die3);
+        die4 = new createjs.Bitmap("Assets/images/4.png");
+        die4.x = 400;
+        die4.y = 100;
+        stage.addChild(die4);
+        die5 = new createjs.Bitmap("Assets/images/5.png");
+        die5.x = 400;
+        die5.y = 100;
+        stage.addChild(die5);
+        die6 = new createjs.Bitmap("Assets/images/6.png");
+        die6.x = 400;
+        die6.y = 100;
         rollButton.on("click", rollButton_clicked);
     }
     window.addEventListener("load", init);
