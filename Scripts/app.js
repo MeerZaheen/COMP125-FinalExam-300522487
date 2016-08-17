0358520e01a0e412e7d191f7f24cc437f17b19cc
MeerZaheen <MeerZ@Meers-MacBook-Pro.local>
2016-08-17 14:11:37 -0400

    added rollButton Clicked

--- Scripts/app.js
+++ Scripts/app.js
@@ -16,6 +16,8 @@
 var core;
 (function (core) {
     "use strict";
+    var randomNumber1 = 0;
+    var randomNumber2 = 0;
     var canvas;
     var stage;
     var rollButton;
@@ -40,10 +42,10 @@ var core;
         stage.update();
     }
     function rollButton_clicked() {
-        die1 = Math.floor((Math.random() * 6) + 1);
-        console.log(die1);
-        die2 = Math.floor((Math.random() * 6) + 1);
-        console.log(die1);
+        rollButton1 = Math.floor((Math.random() * 6) + 1);
+        console.log(rollButton);
+        rollButton2 = Math.floor((Math.random() * 6) + 1);
+        console.log(rollButton);
     }
     function main() {
 
@@ -54,35 +56,11 @@ var core;
         blank2 = new createjs.Bitmap("Assets/images/blank.png");
         blank2.x = 100;
         blank2.y = 100;
-
         stage.addChild(blank2);
         blank = new createjs.Bitmap("Assets/images/blank.png");
         blank.x = 400;
         blank.y = 100;
         stage.addChild(blank);
-        die1 = new createjs.Bitmap("Assets/images/1.png");
-        die1.x = 400;
-        die1.y = 100;
-        stage.addChild(die1);
-        die2 = new createjs.Bitmap("Assets/images/2.png");
-        die2.x = 400;
-        die2.y = 100;
-        stage.addChild(die2);
-        die3 = new createjs.Bitmap("Assets/images/3.png");
-        die3.x = 400;
-        die3.y = 100;
-        stage.addChild(die3);
-        die4 = new createjs.Bitmap("Assets/images/4.png");
-        die4.x = 400;
-        die4.y = 100;
-        stage.addChild(die4);
-        die5 = new createjs.Bitmap("Assets/images/5.png");
-        die5.x = 400;
-        die5.y = 100;
-        stage.addChild(die5);
-        die6 = new createjs.Bitmap("Assets/images/6.png");
-        die6.x = 400;
-        die6.y = 100;
         rollButton.on("click", rollButton_clicked);
     }
     window.addEventListener("load", init);
